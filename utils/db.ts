b457e9ffe1b3aa5cd1f4c6cb142bed9dd979a911
ramez <ramezgamel52@gmail.com>
2024-10-29 04:08:06 +0300

    update prisma instance

--- utils/db.ts
+++ utils/db.ts
@@ -10,15 +10,14 @@
 
 import { PrismaClient } from "@prisma/client";
 
-let prisma;
+declare global {
+  var prisma: PrismaClient | undefined;
+}
 
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-  prisma = global.prisma;
+const prisma = global.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+  global.prisma = prisma;
 }
 
 export default prisma;
