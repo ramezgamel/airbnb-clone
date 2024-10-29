99649b5c18a22220fd43211e0bfea4a114f3fa9f
ramez <ramezgamel52@gmail.com>
2024-10-29 04:03:51 +0300

    update prisma instance

--- utils/db.ts
+++ utils/db.ts
@@ -1,9 +1,24 @@
+// import { PrismaClient } from "@prisma/client";
+// const prismaClientSingleton = () => new PrismaClient();
+// type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
+// const globalForPrisma = globalThis as unknown as {
+//   prisma: PrismaClientSingleton | undefined;
+// };
+// const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
+// export default prisma;
+// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
 import { PrismaClient } from "@prisma/client";
-const prismaClientSingleton = () => new PrismaClient();
-type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClientSingleton | undefined;
-};
-const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
+
+let prisma;
+
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
+
 export default prisma;
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
